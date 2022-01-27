0d8f8d80ec5f18960b3129844746462182e35c74
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-01-27 21:57:59 +0200

    login

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -9,4 +9,9 @@ router.post('/register', async (req, res) => {
   res.status(200).send({ user });
 });
 
+router.post('/login', async (req, res) => {
+  const user = await userService.login(req.body);
+  res.status(200).send({ user });
+});
+
 module.exports = router;
--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,4 +1,5 @@
-const { hashPassword } = require('../helper/password');
+const _ = require('lodash');
+const { hashPassword, compareHashedPassword } = require('../helper/password');
 const { createToken } = require('../helper/token');
 const UserModel = require('./user.schema');
 
@@ -23,9 +24,35 @@ class UserService {
     const hashedPassword = await hashPassword(password);
 
     const createdUser = await this.model.create({ ...args, password: hashedPassword });
-    const token = createToken(createdUser.toObject());
 
-    return { ...createdUser.toObject(), token };
+    const token = createToken(_.omit(createdUser.toObject(), ['password']));
+
+    return { ..._.omit(createdUser.toObject(), ['password']), token };
+  }
+
+  /**
+   *
+   * @param {Object} args
+   * @param {String} args.email
+   * @param {String} args.password
+   */
+  async login(args) {
+    const { email, password } = args;
+    const user = await this.getByEmail(email);
+
+    if (!user) {
+      throw new Error('User not exists');
+    }
+
+    const passwordIsMatch = await compareHashedPassword(password, user.password);
+
+    if (!passwordIsMatch) {
+      throw new Error('Password not correct');
+    }
+
+    const token = createToken(_.omit(user, ['password']));
+
+    return { ..._.omit(user, ['password']), token };
   }
 }
 
