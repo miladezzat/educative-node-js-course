29bd8d3d582c1d411b4b4829ba1ace28ee0f453d
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-01-27 22:06:52 +0200

    check auth middleware

---
+++ src/middleware/check-auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const { tokenPublicSecreteKey, tokenSignOptions } = require('../../config/app-config');
+
+const checkAuth = (req, res, next) => {
+  const token = req.header('x-auth-token') || req.header('token');
+
+  if (!token) {
+    throw new Error('authorization denied');
+  }
+
+  try {
+    req.user = jwt.verify(token, tokenPublicSecreteKey, tokenSignOptions);
+
+    next();
+  } catch (error) {
+    throw new Error('authorization denied');
+  }
+};
+module.exports = checkAuth;
