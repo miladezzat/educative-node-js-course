35e1e711adf469e417f8ed99392af3e1512e813f
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-01-28 11:52:27 +0200

    me api endpoint

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userService = require('./user.service');
+const checkAuth = require('../middleware/check-auth.middleware');
 
 const router = new express.Router();
 
@@ -12,6 +13,10 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   const user = await userService.login(req.body);
   res.status(200).send({ user });
+});
+
+router.get('/me', checkAuth, async (req, res) => {
+  res.status(200).send({ user: req.user });
 });
 
 module.exports = router;
